bfdd590a5a2baa28084cfef98192c4f91809a354
Hamza <hamzazubair228@gmail.com>
2023-06-08 07:02:18 -0700

    adding home page

--- src/components/LandingPage/HomeMain/HomeMain.js
+++ src/components/LandingPage/HomeMain/HomeMain.js
@@ -1,38 +1,75 @@
-import React from "react";
-import { Main } from "./HomeMain.styles";
+import React, { useState, useRef } from "react";
+import { Main, ResponsiveImage } from "./HomeMain.styles";
 import { Container } from "src/components/Container.styles";
 import Image from "next/image";
-import Poster from "../../../assets/images/Homemain/Main-poster.png";
-import Banner from "../../../assets/images/Homemain/Main-poster-1.png";
-import Baner from "../../../assets/images/Homemain/Main-poster-2.png";
-import { Flex } from "src/components/Flex.styles";
-
+import thumbnail from "../../../assets/images/HomeMain/video-pic.png";
 const HomeMain = () => {
+  const videoRef = useRef(null);
+  const [toggle, setToggle] = useState(true);
+  const handleVideo = () => {
+    setTimeout(() => {
+      setToggle(true);
+    }, 22000);
+    setTimeout(() => {
+      setToggle(false);
+    }, 1000);
+  };
+  const [clicked, setClicked] = useState(false);
+
+  const handleButtonClick = () => {
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+    }, 22000);
+  };
+
   return (
     <Main>
-      <Container className="container">
-        <Flex sm>
+      {toggle && (
+        <Container className="container">
           <div>
             <h1>
-              Grow your Business Today with <br /> Brand Strategy.{" "}
+              Grow your Business Today with <br />
+              Brand Strategy.
             </h1>
             <p>
-              Build your Search Engine Optimization Foundation The Trusted
-              Experts. We Offer The Right Plans.
+              Build your Search Engine Optimization Foundation <br /> The
+              Trusted Experts. We Offer The Right Plans.
             </p>
             <div className="btn">
               <button>GET STARTED</button>
             </div>
           </div>
-          <Image className="Poster" src={Poster} alt="Poster" />
-          <div className="Banner" >
-            <Image src={Banner} alt="Poster" />
+
+          <div
+            className="video"
+            onClick={handleVideo}
+            style={{ transform: clicked ? "translateY(-320px)" : "none" }}
+          >
+            <ResponsiveImage
+              src={thumbnail}
+              onClick={handleButtonClick}
+              max={"1030"}
+              min="350"
+              minh="171.94"
+              maxh="506"
+              alt="video-image"
+            />
           </div>
-          <div className="Baner">
-          <Image className="Baner" src={Baner} alt="Poster" />
-          </div>
-        </Flex>
-      </Container>
+        </Container>
+      )}
+      {!toggle && (
+        <div>
+          {/* onClick={() => setToggle(true)} */}
+          <video ref={videoRef} autoPlay>
+            <source
+              src="/Videos/video-1.mp4"
+              type="video/mp4"
+              onClick={() => setToggle(false)}
+            />
+          </video>
+        </div>
+      )}
     </Main>
   );
 };
