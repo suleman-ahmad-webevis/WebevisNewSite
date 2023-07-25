6c0b70a4ba1edd35b8af42c72c7404a6285fcf8f
Hamza <hamzazubair228@gmail.com>
2023-07-25 19:09:02 +0500

    updating navbar

--- src/components/ExpertPool/Home/Home.styles.js
+++ src/components/ExpertPool/Home/Home.styles.js
@@ -124,7 +124,7 @@ export const ImageWrapper = styled(Image)`
   /* display: inline-block; */
   max-width: 100%;
   height: auto;
-  margin: 0 141px -46px auto;
+  margin: 0 141px -85px auto;
 `;
 export const DevInfoWrapper = styled.div`
   position: relative;
