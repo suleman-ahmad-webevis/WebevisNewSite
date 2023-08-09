dec7807895061ba903fd7a70779b581259733b54
Hamza Zubair <mailto:hamza@webevis.com>
2023-08-09 15:31:41 +0500

    updating development with satging

--- src/components/ContactUs/MessageUs/MessageUs.js
+++ src/components/ContactUs/MessageUs/MessageUs.js
@@ -29,137 +29,122 @@ const validationSchema = Yup.object().shape({
 
 const MessageUs = () => {
   const [isCaptchaCompleted, setIsCaptchaCompleted] = useState(false);
-
-  const handleSubmit = (values, { resetForm }) => {
-    if (isCaptchaCompleted) {
-      console.log(values);
-      // clear form
-      resetForm();
-    } else {
-      console.log("reCAPTCHA challenge not completed. Form not submitted.");
-    }
-  };
-  const key =
-    process.env.ReCAPTCHA_KEY || "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
-
-  function onChange(value) {
-    console.log("Captcha value:", value);
-    setIsCaptchaCompleted(true); // Set the state when reCAPTCHA is completed
+const [recaptchaKey, setRecaptchaKey] = useState(
+  "6LcgPZEnAAAAANJFAnpOgd94CWSs5KVVGdPml1Mn"
+);
+const handleSubmit = (values, { resetForm }) => {
+  if (isCaptchaCompleted) {
+    console.log(values);
+    // clear form
+    resetForm();
+  } else {
+    console.log("reCAPTCHA challenge not completed. Form not submitted.");
   }
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-    >
-      <MessageContainer>
-        <Container resPadding="0px">
-          <Message>
-            <h1>Send us a message</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit{" "}
-              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-            </p>
-            <Form>
-              <div className="input-wrap">
-                <div className="fields">
-                  <label htmlFor="name">Name</label>
-                  <Field type="text" id="name" name="name" placeholder="Name" />
-                  <ErrorMessage name="name" component="div" className="error" />
-                </div>
-                <div className="fields">
-                  <label htmlFor="company">Company</label>
-                  <Field
-                    type="text"
-                    id="company"
-                    name="company"
-                    placeholder="Webevis"
-                  />
-                  <ErrorMessage
-                    name="company"
-                    component="div"
-                    className="error"
-                  />
-                </div>
-              </div>
-              <div className="input-wrap">
-                <div className="fields">
-                  <label htmlFor="phone">Phone</label>
-                  <Field
-                    type="text"
-                    id="phone"
-                    name="phone"
-                    placeholder="210498230573"
-                  />
-                  <ErrorMessage
-                    name="phone"
-                    component="div"
-                    className="error"
-                  />
-                </div>
-                <div className="fields">
-                  <label htmlFor="email">Email</label>
-                  <Field
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder="user@gmail.com"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="error"
-                  />
-                </div>
+};
+// const key = process.env.RECAPTCHA_KEY;
+// console.log("RECAPTCHA_KEY:", key);
+function onChange(value) {
+  console.log("Captcha value:", value);
+  setIsCaptchaCompleted(true); // Set the state when reCAPTCHA is completed
+}
+return (
+  <Formik
+    initialValues={initialValues}
+    validationSchema={validationSchema}
+    onSubmit={handleSubmit}
+  >
+    <MessageContainer>
+      <Container resPadding="0px">
+        <Message>
+          <h1>Send us a message</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit{" "}
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+          <Form>
+            <div className="input-wrap">
+              <div className="fields">
+                <label htmlFor="name">Name</label>
+                <Field type="text" id="name" name="name" placeholder="Name" />
+                <ErrorMessage name="name" component="div" className="error" />
               </div>
               <div className="fields">
-                <label htmlFor="subject">Subject</label>
+                <label htmlFor="company">Company</label>
                 <Field
                   type="text"
-                  id="subject"
-                  name="subject"
-                  placeholder="Seo"
+                  id="company"
+                  name="company"
+                  placeholder="Webevis"
                 />
                 <ErrorMessage
-                  name="subject"
+                  name="company"
                   component="div"
                   className="error"
                 />
+              </div>
+            </div>
+            <div className="input-wrap">
+              <div className="fields">
+                <label htmlFor="phone">Phone</label>
+                <Field
+                  type="text"
+                  id="phone"
+                  name="phone"
+                  placeholder="210498230573"
+                />
+                <ErrorMessage name="phone" component="div" className="error" />
               </div>
               <div className="fields">
-                <label htmlFor="message">Message</label>
+                <label htmlFor="email">Email</label>
                 <Field
-                  as="textarea"
-                  id="message"
-                  name="message"
-                  placeholder="Message"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="user@gmail.com"
                 />
-                <ErrorMessage
-                  name="message"
-                  component="div"
-                  className="error"
-                />
+                <ErrorMessage name="email" component="div" className="error" />
               </div>
-              <ReCAPTCHA sitekey={key} onChange={onChange} />
-              <PrimaryButton
-                shadowH="none"
-                minWidth="327.019"
-                height="50"
-                minheight="40"
-                size="24"
-                minsize="16"
-                weight="700"
-                radius="3px"
-                disabled={!isCaptchaCompleted}
-              >
-                Send Message
-              </PrimaryButton>
-            </Form>
-          </Message>
-        </Container>
-      </MessageContainer>
-    </Formik>
-  );
+            </div>
+            <div className="fields">
+              <label htmlFor="subject">Subject</label>
+              <Field
+                type="text"
+                id="subject"
+                name="subject"
+                placeholder="Seo"
+              />
+              <ErrorMessage name="subject" component="div" className="error" />
+            </div>
+            <div className="fields">
+              <label htmlFor="message">Message</label>
+              <Field
+                as="textarea"
+                id="message"
+                name="message"
+                placeholder="Message"
+              />
+              <ErrorMessage name="message" component="div" className="error" />
+            </div>
+            <ReCAPTCHA sitekey={recaptchaKey} onChange={onChange} />
+            <PrimaryButton
+              shadowH="none"
+              minWidth="327.019"
+              height="50"
+              minheight="40"
+              size="24"
+              minsize="16"
+              weight="700"
+              radius="3px"
+              disabled={!isCaptchaCompleted}
+            >
+              Send Message
+            </PrimaryButton>
+          </Form>
+        </Message>
+      </Container>
+    </MessageContainer>
+  </Formik>
+);
 };
 
 export default MessageUs;
