fe7c5fbc9f3b1890c55ffeadc08d25aa60a659f4
imatif01 <imatif01@gmail.com>
2023-08-09 19:48:44 +0500

    update seo expert

--- src/components/SeoExpert/ExpertForm/ExpertForm.styles.js
+++ src/components/SeoExpert/ExpertForm/ExpertForm.styles.js
@@ -5,7 +5,7 @@ export const ExpertHolder = styled.section`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  padding: 40px;
+  padding: 40px 0;
   p {
     color: #a1a1a1;
   }
@@ -25,7 +25,11 @@ export const Expert = styled.div`
       }
       @media screen and (max-width: 767px) {
         text-align: start;
+        margin-bottom: 20px;
       }
+    }
+    @media screen and (max-width: 767px) {
+      text-align: start;
     }
     p {
       font-size: 24px;
@@ -40,8 +44,9 @@ export const Expert = styled.div`
   .Grid {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    column-gap: 40px;
+    column-gap: 50px;
     row-gap: 30px;
+
     @media screen and (max-width: 991px) {
       grid-template-columns: 1fr 1fr;
     }
@@ -50,6 +55,7 @@ export const Expert = styled.div`
     }
     @media screen and (max-width: 575px) {
       grid-template-columns: 1fr;
+      padding-left: 20px;
     }
     h3 {
       color: #fff;
@@ -57,11 +63,30 @@ export const Expert = styled.div`
       font-weight: 600;
       line-height: 28px;
       margin-bottom: 10px;
+      @media screen and (max-width: 1440px) {
+        font-size: calc(18px + (24 - 18) * (100vw - 390px) / (1440 - 390));
+      }
     }
     p {
       font-size: 18px;
       font-weight: 400;
       line-height: 22px;
+      @media screen and (max-width: 1440px) {
+        font-size: calc(16px + (24 - 16) * (100vw - 390px) / (1440 - 390));
+      }
+    }
+    .Col {
+      position: relative;
+      &::before {
+        content: "";
+        position: absolute;
+        width: 10px;
+        height: 10px;
+        background: #28b781;
+        top: 10px;
+        left: -22px;
+        border-radius: 100%;
+      }
     }
   }
 `;
