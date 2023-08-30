7e7252eb5ba27d266e9b70afe89c66609462f318
Hamza Zubair <mailto:hamza@webevis.com>
2023-08-30 17:01:35 +0500

    updating youTube page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -39,7 +39,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
-
+          <link
+            href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
