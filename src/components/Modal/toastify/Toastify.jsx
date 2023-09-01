afde8c6e8171ca97f8eb57b25b98aed5ac428611
Hamza Zubair <mailto:hamza@webevis.com>
2023-09-01 20:41:16 +0500

    updating development

---
+++ src/components/Modal/toastify/Toastify.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Toastify = () => {
+  return <div>Toastify</div>;
+};
+
+export default Toastify;
--- src/components/WebApp/heroSection/Hero.jsx
+++ src/components/WebApp/heroSection/Hero.jsx
@@ -10,14 +10,25 @@ const Hero = () => {
     <Container>
       <HeroSec>
         <HeroText>
-          <h1>Web Application <br />  Development <br />  Services</h1>
+          <h1>
+            <span className="Text-gradient"> Web Application</span> <br />
+            Development <br /> Services
+          </h1>
           <p>
-            Web application development services provided by <br />  Webevis
-            Technologies yield benefits to companies of <br />  any type or size.
-            Customers get killer software <br />  products that maximize the value of
-            their offerings <br />  and increase the profitability of their business.
+            Web application development services provided by <br /> Webevis
+            Technologies yield benefits to companies of <br /> any type or size.
+            Customers get killer software <br /> products that maximize the
+            value of their offerings <br /> and increase the profitability of
+            their business.
           </p>
-          <PrimaryButton size="24" width="291" height="51" minWidth="217" radius="40px" minsize="16" >
+          <PrimaryButton
+            size="24"
+            width="291"
+            height="51"
+            minWidth="217"
+            radius="40px"
+            minsize="16"
+          >
             Let’s Get Started
           </PrimaryButton>
         </HeroText>
