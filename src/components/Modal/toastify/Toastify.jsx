4f1255c43d4897a24dad0fe353a3fe86a6ece0bf
Hamza Zubair <mailto:hamza@webevis.com>
2023-09-05 16:57:00 +0500

    updating

--- src/components/Modal/toastify/Toastify.jsx
+++ src/components/Modal/toastify/Toastify.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import {
   CloseButton,
   StyledToastify,
+  ToastConatiner,
   ToastImage,
   ToastText,
 } from "./Toastify.styles";
@@ -12,46 +13,49 @@ import infoImage from "../../../../public/assets/images/toastify/infoImage.svg";
 import { RxCross2 } from "react-icons/rx";
 import Image from "next/image";
 const Toastify = ({ open, setOpen, error, info, warning, success, text }) => {
-  if (open) {
-    setTimeout(() => {
-      setOpen(false);
-    }, 10000);
-  }
+  // if (open) {
+  //   setTimeout(() => {
+  //     setOpen(false);
+  //   }, 10000);
+  // }
 
   return (
-    <StyledToastify
-      toast={open}
-      info={info}
-      error={error}
-      warning={warning}
-      success={success}
-    >
-      {info && (
-        <ToastImage>
-          <Image src={infoImage} alt="infoImage" />
-        </ToastImage>
-      )}
-      {error && (
-        <ToastImage>
-          <Image src={errorImage} alt="error" />
-        </ToastImage>
-      )}
-      {warning && (
-        <ToastImage>
-          <Image src={warningImage} alt="warningImage" />
-        </ToastImage>
-      )}
-      {success && (
-        <ToastImage>
-          <Image src={successImage} alt="successImage" />
-        </ToastImage>
-      )}
+    <ToastConatiner>
+      <StyledToastify
+        toast={open}
+        info={info}
+        error={error}
+        warning={warning}
+        success={success}
+        className={open ? "show" : "hide"}
+      >
+        {info && (
+          <ToastImage>
+            <Image src={infoImage} alt="infoImage" />
+          </ToastImage>
+        )}
+        {error && (
+          <ToastImage>
+            <Image src={errorImage} alt="error" />
+          </ToastImage>
+        )}
+        {warning && (
+          <ToastImage>
+            <Image src={warningImage} alt="warningImage" />
+          </ToastImage>
+        )}
+        {success && (
+          <ToastImage>
+            <Image src={successImage} alt="successImage" />
+          </ToastImage>
+        )}
 
-      <ToastText>{text}</ToastText>
-      <CloseButton onClick={() => setOpen(false)}>
-        <RxCross2 />
-      </CloseButton>
-    </StyledToastify>
+        <ToastText>{text}</ToastText>
+        <CloseButton onClick={() => setOpen(false)}>
+          <RxCross2 />
+        </CloseButton>
+      </StyledToastify>
+    </ToastConatiner>
   );
 };
 
